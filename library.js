a26ae5aa87e87981548667dfd27c16a72791e3cb
Marco Von Ballmoos <marco@earthli.com>
2021-11-14 16:01:32 +0100

    Convert to ES6 modules

--- library.js
+++ library.js
@@ -4,31 +4,22 @@ let _counter = 1;
 let m = 2;
 show(_counter + m);
 
-function counter()
+export function counter()
 {
   return _counter;
 }
 
-function increment()
+export function increment()
 {
   return ++_counter;
 }
 
-function decrement()
+export function decrement()
 {
   return --_counter;
 }
 
-function show(value)
+export function show(value)
 {
-  console.log(value);
-}
-
-// TODO Re-export methods with ES6 Modules
-// https://trello.com/c/6dpjfKIP/6-fix-js-module-exports-so-it-works-in-the-browser
-
-module.exports = {
-  counter,
-  increment,
-  decrement
+  console.warn(value);
 }
