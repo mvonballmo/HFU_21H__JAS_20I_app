37fa4c77fe1a1d17321bb4e74b73faac89bec824
Marco Von Ballmoos <marco@earthli.com>
2021-11-15 09:05:47 +0100

    Use Jest test cases with Bio function

--- basics.test.js
+++ basics.test.js
@@ -1,20 +1,29 @@
 const { test, expect, describe } = require('@jest/globals');
 
 describe('Basic functions and operators', () => {
-  test('Bio single year', () => {
-    function Bio(template, name, age) {
-      const years = (age == 1) ? " year" : " years";
 
-      return `${template[0]}${name}${template[1]}${age}${years}${template[2]}`;
+  const bioCases = [
+    ["bob", -2, "The user \"bob\" is -2 years old."],
+    ["bob", -1, "The user \"bob\" is -1 years old."],
+    ["bob", 0, "The user \"bob\" is 0 years old."],
+    ["john", 1, "The user \"john\" is 1 year old."],
+    ["john", 2, "The user \"john\" is 2 years old."]
+  ];
+
+  test.each(bioCases)(
+    "given %p and %p, Bio returns %p",
+    (argOne, argTwo, expectedResult) => {
+      function Bio(t, name, age) {
+        const years = (age == 1) ? " year" : " years";
+
+        return `${t[0]}${name}${t[1]}${age}${years}${t[2]}`;
+      }
+
+      const output = Bio`The user "${argOne}" is ${argTwo} old.`;
+
+      expect(output).toEqual(expectedResult);
     }
-
-    let age = 0;
-    let person = "bob";
-
-    const output = Bio`The person named ${person} is ${age} old.`;
-
-    expect(output).toBe("The person named bob is 0 years old.");
-  })
+  );
 
   test('var uses same variable', () => {
     var x = 1;
