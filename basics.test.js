9a89ed7de3563f042715576977e91034b71b95df
Marco Von Ballmoos <marco@earthli.com>
2021-11-15 20:35:59 +0100

    Add tests for coalescing, comparison, spread

--- basics.test.js
+++ basics.test.js
@@ -146,4 +146,89 @@ describe('Basic functions and operators', () => {
       console.log(key + ' = ' + value)
     }
   })
+
+  test('null-checking without coalescing operators', () => {
+    const person = {
+      first: "bob",
+      last: "Hoffman",
+      age: 34,
+      company: {
+        name: "Apple"
+      }
+    };
+
+    if (person) {
+      if (person.company) {
+        if (person.company.name != null) {
+          const z = person.company.name;
+          expect(z).toBe("Apple");
+        } else {
+          throw new Error("company.name is not assigned");
+        }
+      } else {
+        throw new Error("company is not assigned");
+      }
+    } else {
+      throw new Error("person is not assigned");
+    }
+  })
+
+  test('null-checking with coalescing operators', () => {
+    const person = {
+      first: "bob",
+      last: "Hoffman",
+      age: 34,
+      company: {
+        name: "Apple"
+      }
+    };
+
+    const person2 = null;
+    const person3 = {
+      first: "bob",
+      last: "Hoffman",
+      age: 34,
+    }
+
+    expect(person.company.name).toBe("Apple");
+    expect(person2?.company?.name).toBe(undefined);
+    expect(person3.company?.name).toBe(undefined);
+  })
+
+  test("test comparison operators", () =>{
+    const x = 1 == '1';   // x ist 'true'
+    const y = 1 === '1';  // y ist 'false'
+
+    expect(x).toBeTruthy();
+    expect(y).toBeFalsy();
+  })
+
+  test("spread operator with parameters", () => {
+    function sum(x, y, z) {
+      return x + y + z;
+    }
+    const numbers = [1, 2, 3];
+    const total = sum(...numbers);  // Ergibt 6
+
+    expect(total).toBe(6);
+  })
+
+  test("destructuring", () => {
+    const items = [10, 20, 30, 40, 50];
+    const [a, b, ...rest] = items;
+// a == 10, b == 20, rest == [30, 40, 50]
+
+    expect(a).toEqual(10)
+    expect(b).toEqual(20)
+    expect(rest).toEqual([30, 40, 50])
+
+// Mit Standardwerte
+    const [c, d, e = -1, f = -1] = rest;
+// c == 30, d == 40, e = 50, f = -1
+    expect(c).toEqual(30)
+    expect(d).toEqual(40)
+    expect(e).toEqual(50)
+    expect(f).toEqual(-1)
+  })
+
 })
