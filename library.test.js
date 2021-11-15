669dd319585c5594d4798a4b27e5ab009d144dc6
Marco Von Ballmoos <marco@earthli.com>
2021-11-15 08:48:37 +0100

    Describe the library tests

--- library.test.js
+++ library.test.js
@@ -1,18 +1,21 @@
-const { test } = require('@jest/globals');
+const { test, describe } = require('@jest/globals');
 const library = require('./library');
 
-test('counter is initialized to 1', () => {
-  expect(library.counter()).toBe(1);
-});
+describe('library', () => {
+  test('counter is initialized to 1', () => {
+    expect(library.counter()).toBe(1);
+  });
 
-test('increment increases by 1', () => {
-  const currentCounter = library.counter();
+  test('increment increases by 1', () => {
+    const currentCounter = library.counter();
 
-  expect(library.increment()).toBe(currentCounter + 1);
-});
+    expect(library.increment()).toBe(currentCounter + 1);
+  });
 
-test('decrement decreases by 1', () => {
-  const currentCounter = library.counter();
+  test('decrement decreases by 1', () => {
+    const currentCounter = library.counter();
 
-  expect(library.decrement()).toBe(currentCounter - 1);
-});
+    expect(library.decrement()).toBe(currentCounter - 1);
+  });
+})
+
