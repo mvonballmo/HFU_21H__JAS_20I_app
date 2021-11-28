88ebc46730980b3300cbc5d5a0533e5c85a54c49
Marco Von Ballmoos <marco@earthli.com>
2021-11-28 09:48:00 +0100

    Add test for calling fetch

---
+++ tests/fetch.test.js
@@ -0,0 +1,12 @@
+import { describe, expect, test } from "@jest/globals";
+import "isomorphic-fetch";
+
+describe("Fetch", () => {
+  test("call fetch gets addresses", () => {
+    fetch("http://localhost:3000/addresses")
+      .then(data => data.json())
+      .then(addresses => {
+        expect(addresses.length).toBe(34);
+      });
+  });
+});
