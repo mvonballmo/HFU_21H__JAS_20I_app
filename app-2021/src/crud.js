b3e59bbcc3c94f9107ea80d0ed454334b77cb79c
Marco Von Ballmoos <marco@earthli.com>
2021-11-28 22:56:28 +0100

    Improve JSDoc for crud.js

--- app-2021/src/crud.js
+++ app-2021/src/crud.js
@@ -14,7 +14,7 @@ export class crud {
   /**
    * Gets all entities from the database.
    *
-   * @return Promise<[{ id: number }]>
+   * @return {Promise<{ id: number }>}
    */
   getAll() {
     return this.#execute(this._rootUrl);
@@ -24,6 +24,7 @@ export class crud {
    * Gets the entity with the given `id` from the database.
    *
    * @param {number} id The unique identifier for the requested entity.
+   * @return {Promise<{ id: number }>}
    */
   get(id) {
     return this.#execute(`${this._rootUrl}/${id}`);
@@ -33,7 +34,7 @@ export class crud {
    * Inserts the given `entity` into the database.
    *
    * @param {{}} entity The object to insert.
-   * @return The inserted object with the ID applied.
+   * @return {Promise<{ id: number }>} The inserted object with the ID applied.
    */
   insert(entity) {
     return this.#insertOrUpdate(`${this._rootUrl}/`, entity, "POST");
@@ -43,6 +44,7 @@ export class crud {
    * Updates the given `entity` in the database.
    *
    * @param {{ id: number }} entity The object to update.
+   * @return {Promise<{ id: number }>} The updated object
    */
   update(entity) {
     return this.#insertOrUpdate(`${this._rootUrl}/${entity.id}`, entity, "PUT");
@@ -52,17 +54,29 @@ export class crud {
    * Deletes the given `entity` from the database.
    *
    * @param {{ id: number }} entity The object to delete.
+   * @return {Promise<Response>} A promise for the delete task.
    */
   delete(entity) {
     return fetch(`${this._rootUrl}/${entity.id}`, { method: "DELETE" });
   }
 
+  /**
+   * @param {string} url
+   * @param {RequestInit} init
+   * @return {Promise<any>}
+   */
   async #execute(url, init = undefined) {
     const response = await fetch(url, init);
 
     return response.json();
   }
 
+  /**
+   * @param {string} url
+   * @param {{}} address
+   * @param {string} method
+   * @return {Promise<any>} A promise for the delete task.
+   */
   #insertOrUpdate(url, address, method) {
     return this.#execute(url, {
       method: method,
@@ -74,8 +88,6 @@ export class crud {
   }
 
   /**
-   * The root url to use for all entity commands.
-   *
    * @type string
    */
   #_rootUrl;
