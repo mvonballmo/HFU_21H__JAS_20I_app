cff2b9f45d0ce839021daa7ac30ef376284954a0
Marco Von Ballmoos <marco@earthli.com>
2021-11-28 09:48:13 +0100

    Add test that uses jest's "done" construct

--- tests/fetch.test.js
+++ tests/fetch.test.js
@@ -9,4 +9,14 @@ describe("Fetch", () => {
         expect(addresses.length).toBe(34);
       });
   });
+
+  test("call fetch gets addresses with done", done => {
+    fetch("http://localhost:3000/addresses")
+      .then(data => data.json())
+      .then(addresses => {
+        expect(addresses.length).toBe(34);
+        done();
+      })
+      .catch(error => done(error));
+  });
 });
