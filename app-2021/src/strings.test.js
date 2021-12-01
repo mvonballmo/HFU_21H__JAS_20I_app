289daf7202af6d00f1609e91a89273122412940d
Marco Von Ballmoos <marco@earthli.com>
2021-12-01 17:16:36 +0100

    Add support for multiple search strings

--- app-2021/src/strings.test.js
+++ app-2021/src/strings.test.js
@@ -263,21 +263,30 @@ describe("Basic functions and operators", () => {
       },
     };
 
-    function* findText(obj, searchText) {
+    /**
+     *
+     * @param {any} obj
+     * @param {...string} searchTexts
+     * @return {[{object: any, propertyName: string, value: string, searchText: string}]}
+     */
+    function* findText(obj, ...searchTexts) {
       for (const key in obj) {
         const value = obj[key];
 
         if (typeof value === "object") {
-          yield* findText(value, searchText);
+          yield* findText(value, ...searchTexts);
         } else {
           const textValue = value.toString();
 
-          if (textValue.includes(searchText)) {
-            yield {
-              object: obj,
-              propertyName: key,
-              value: textValue,
-            };
+          for (const searchText of searchTexts) {
+            if (textValue.includes(searchText)) {
+              yield {
+                object: obj,
+                propertyName: key,
+                value: textValue,
+                searchText,
+              };
+            }
           }
         }
       }
@@ -285,25 +294,33 @@ describe("Basic functions and operators", () => {
 
     const [...hoffmanResults] = findText(person, "Hoff");
 
-    expect(hoffmanResults).toEqual([{ object: person, propertyName: "last", value: "Hoffman" }]);
+    expect(hoffmanResults).toEqual([{ object: person, propertyName: "last", value: "Hoffman", searchText: "Hoff" }]);
 
     const [...oResults] = findText(person, "o");
 
     expect(oResults).toEqual([
-      { object: person, propertyName: "first", value: "Bob" },
-      { object: person, propertyName: "middle", value: "Joe" },
-      { object: person, propertyName: "last", value: "Hoffman" },
-      { object: person.company, propertyName: "role", value: "software developer" },
-      { object: person.company.projects[0], propertyName: "role", value: "software developer" },
-      { object: person.company.projects[1], propertyName: "name", value: "MacBook Pro" },
-      { object: person.company.projects[1], propertyName: "role", value: "project manager" },
+      { object: person, propertyName: "first", value: "Bob", searchText: "o" },
+      { object: person, propertyName: "middle", value: "Joe", searchText: "o" },
+      { object: person, propertyName: "last", value: "Hoffman", searchText: "o" },
+      { object: person.company, propertyName: "role", value: "software developer", searchText: "o" },
+      { object: person.company.projects[0], propertyName: "role", value: "software developer", searchText: "o" },
+      { object: person.company.projects[1], propertyName: "name", value: "MacBook Pro", searchText: "o" },
+      { object: person.company.projects[1], propertyName: "role", value: "project manager", searchText: "o" },
     ]);
 
     const [...softwareResults] = findText(person, "software");
 
     expect(softwareResults).toEqual([
-      { object: person.company, propertyName: "role", value: "software developer" },
-      { object: person.company.projects[0], propertyName: "role", value: "software developer" },
+      { object: person.company, propertyName: "role", value: "software developer", searchText: "software" },
+      { object: person.company.projects[0], propertyName: "role", value: "software developer", searchText: "software" },
+    ]);
+
+    const [...multiResults] = findText(person, "Hoff", "dev");
+
+    expect(multiResults).toEqual([
+      { object: person, propertyName: "last", value: "Hoffman", searchText: "Hoff" },
+      { object: person.company, propertyName: "role", value: "software developer", searchText: "dev" },
+      { object: person.company.projects[0], propertyName: "role", value: "software developer", searchText: "dev" },
     ]);
   });
 });
