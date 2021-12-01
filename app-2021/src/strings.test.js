51c1e3b61e931a168e28939c2846eb84fab29b26
Marco Von Ballmoos <marco@earthli.com>
2021-12-01 16:03:40 +0100

    Add string tests for match() and matchAll()

--- app-2021/src/strings.test.js
+++ app-2021/src/strings.test.js
@@ -84,4 +84,50 @@ describe("Basic functions and operators", () => {
     expect(test.replace(/[^ ]s/, "at")).toBe("That is a sentence.");
     expect(test.replaceAll(/[^ ]s/g, "at")).toBe("That at a sentence.");
   });
+
+  test("match() with single result", () => {
+    const test = "This is a sentence.";
+
+    const match = test.match(/(([^ ])s)/);
+
+    expect(match.index).toBe(2);
+    expect(match.groups).toBeUndefined();
+    expect(match.input).toBe(test);
+    expect(match.length).toBe(3);
+    expect(match[0]).toBe("is"); // Full match
+    expect(match[1]).toBe("is"); // Outer capture group
+    expect(match[2]).toBe("i"); // Inner capture group
+  });
+
+  test("match() with multiple results (global)", () => {
+    const test = "This is a sentence.";
+
+    const matches = test.match(/(([^ ])s)/g);
+
+    expect(matches).toEqual(["is", "is"]);
+  });
+
+  test("matchAll()", () => {
+    const test = "This is a sentence.";
+
+    const [...allMatches] = test.matchAll(/(([^ ])s)/g);
+
+    const match0 = allMatches[0];
+    expect(match0.index).toBe(2);
+    expect(match0.groups).toBeUndefined();
+    expect(match0.input).toBe(test);
+    expect(match0.length).toBe(3);
+    expect(match0[0]).toBe("is");
+    expect(match0[1]).toBe("is");
+    expect(match0[2]).toBe("i");
+
+    const match1 = allMatches[1];
+    expect(match1.index).toBe(5);
+    expect(match1.groups).toBeUndefined();
+    expect(match1.input).toBe(test);
+    expect(match1.length).toBe(3);
+    expect(match1[0]).toBe("is");
+    expect(match1[1]).toBe("is");
+    expect(match1[2]).toBe("i");
+  });
 });
