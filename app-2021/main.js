44745c3b858be650582d3ebc41a157c40c911a34
Marco von Ballmoos <marco@earthli.com>
2021-12-04 22:54:16 +0100

    Remove remaining unneeded buttons

--- app-2021/main.js
+++ app-2021/main.js
@@ -1,17 +1,4 @@
-import { crud } from "./src/crud.js";
-import { address } from "./src/address.js";
 import { application } from "./src/app.js";
-
-document.getElementById("showAddress").addEventListener("click", async () => {
-  /**
-   * @type {crud<address>}
-   */
-  const addresses = new crud("http://localhost:3000/addresses");
-
-  const address = await addresses.get(1);
-
-  console.log(address);
-});
 
 const app = new application("http://localhost:3000/");
 
