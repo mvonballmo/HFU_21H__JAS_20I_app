d3a9909c6ae96b82a573b82aae47eb6e6d3a3212
Marco Von Ballmoos <marco@earthli.com>
2021-12-04 01:11:58 +0100

    Add TypeScript definition for address.js

---
+++ app-2021/src/address.d.ts
@@ -0,0 +1,5 @@
+export class address {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
--- app-2021/src/address.js
+++ app-2021/src/address.js
@@ -1,3 +1,7 @@
+// NOTE: we still need the JSDoc below because WebStorm is not picking up the
+// types of the properties from the address.d.ts file. VSC is doing it
+// correctly.
+
 export class address {
   /**
    * @type number
