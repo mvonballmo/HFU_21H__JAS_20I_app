7af48f40ab48fb45428cb56f0c0210d6d2b14dcb
Marco Von Ballmoos <marco@earthli.com>
2021-12-04 16:26:18 +0100

    Remove testing code and set up app UI structure
    # Conflicts:
    #	app-2021/index.html
    #	app-2021/main.js

--- app-2021/main.js
+++ app-2021/main.js
@@ -1,15 +1,5 @@
-import { show, decrement, increment } from "./src/basics.js";
 import { crud } from "./src/crud.js";
-import { address } from "./src/address.js";
-import { createCarListItemsHtml } from "./src/app.js"; // Needed by VSC
-
-document.getElementById("add").addEventListener("click", async () => {
-  show(increment());
-});
-
-document.getElementById("subtract").addEventListener("click", async () => {
-  show(decrement());
-});
+import { address } from "./src/address.js"; // Needed by VSC
 
 document.getElementById("showAddress").addEventListener("click", async () => {
   /**
@@ -20,36 +10,4 @@ document.getElementById("showAddress").addEventListener("click", async () => {
   const address = await addresses.get(1);
 
   console.log(address);
-  show(address.firstName);
-
-  const listItems = await createCarListItemsHtml();
-  const [nav] = document.getElementsByTagName("nav");
-
-  nav.innerHTML = `<ul>${listItems}</ul>`;
-});
-
-const log = e => {
-  console.log(`${e.target.nodeName}, ${e.currentTarget.nodeName}`);
-  e.stopPropagation();
-};
-
-document.addEventListener("click", log);
-
-const containers = document.querySelectorAll(".buttons");
-for (const container of containers) {
-  container.addEventListener("click", log);
-}
-
-const buttons = document.getElementsByTagName("button");
-for (const button of buttons) {
-  button.addEventListener("click", log);
-}
-
-const duckDuckGo = document.getElementById("search");
-
-duckDuckGo.addEventListener("click", e => {
-  const shouldChangePage = confirm("Are you sure?");
-  if (!shouldChangePage) {
-    e.preventDefault();
-  }
 });
