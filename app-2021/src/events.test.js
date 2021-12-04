6295ce68de719508450a7be5e5a29e3fa7612169
Marco Von Ballmoos <marco@earthli.com>
2021-12-04 11:12:28 +0100

    Add tests for onclick attribute on element

---
+++ app-2021/src/events.test.js
@@ -0,0 +1,63 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, expect, test } from "@jest/globals";
+
+describe("Events", () => {
+  test("using onclick attribute", () => {
+    document.body.innerHTML = `<button id="button" onclick="clickCount += 1"/>`;
+
+    const button = document.getElementById("button");
+
+    window.clickCount = 0;
+
+    button.click();
+
+    expect(window.clickCount).toBe(1);
+  });
+
+  test("using onclick attribute with 'this'", () => {
+    document.body.innerHTML = `<button id="button" onclick="targetId = this.id"/>`;
+
+    window.targetId = "";
+
+    const button = document.getElementById("button");
+
+    button.click();
+
+    expect(window.targetId).toBe("button");
+  });
+
+  test("using onclick attribute with closure", () => {
+    document.body.innerHTML = `<button id="button" onclick="buttonClick(this)"/>`;
+
+    let targetId;
+
+    window.buttonClick = btn => {
+      targetId = btn.id;
+    };
+
+    const button = document.getElementById("button");
+
+    button.click();
+
+    expect(targetId).toBe("button");
+  });
+
+  test("using onclick attribute with event", () => {
+    document.body.innerHTML = `<button id="button" onclick="clicked(event)"/>`;
+
+    let targetId;
+
+    window.clicked = e => {
+      targetId = e.target.id;
+    };
+
+    const button = document.getElementById("button");
+
+    button.click();
+
+    expect(targetId).toBe("button");
+  });
+});
