bf2f64265a6f2d72c772e11ce14b12270c56bbf0
Marco Von Ballmoos <marco@earthli.com>
2021-12-15 23:16:48 +0100

    Refactor createTocHTML from applyToc

--- app-2021/src/toc.js
+++ app-2021/src/toc.js
@@ -1,5 +1,12 @@
 export class tocBuilder {
   applyToc(minLevel = 1, maxLevel = 6) {
+    const tocElement = document.querySelector("#toc");
+    if (tocElement) {
+      tocElement.innerHTML = this.createTocHTML(minLevel, maxLevel);
+    }
+  }
+
+  createTocHTML(minLevel = 1, maxLevel = 6) {
     const allLevels = [...Array(7).keys()];
     const levels = allLevels.slice(minLevel, maxLevel + 1);
     const headingLevels = levels.map(l => `h${l}`);
@@ -7,39 +14,33 @@ export class tocBuilder {
 
     const headings = document.querySelectorAll(headingSelectors);
 
-    let toc = "<ul>";
-    /** @type Element */
+    let result = "<ul>";
     let currentLevel = undefined;
 
-    const getLevel = n => n[1];
-
     for (const heading of headings) {
-      const headingLevel = getLevel(heading.tagName);
+      const headingLevel = heading.tagName[1];
 
       if (currentLevel) {
         if (currentLevel < headingLevel) {
-          toc += "<ul>\n";
+          result += "<ul>\n";
         } else if (currentLevel > headingLevel) {
           for (let index = 0; index < currentLevel; index++) {
-            toc += "</ul>\n";
+            result += "</ul>\n";
           }
           currentLevel = headingLevel;
-          toc += "<ul>";
+          result += "<ul>";
         }
       }
 
       heading.id = heading.textContent.replaceAll(/[^A-Zöäü0-9]/gi, "-");
 
-      toc += `<li><a href="#${heading.id}">${heading.textContent}</a></li>\n`;
+      result += `<li><a href="#${heading.id}">${heading.textContent}</a></li>\n`;
 
       currentLevel = headingLevel;
     }
 
-    toc += "</ul>";
+    result += "</ul>";
 
-    const tocElement = document.querySelector("#toc");
-    if (tocElement) {
-      tocElement.innerHTML = toc;
-    }
+    return result;
   }
 }
