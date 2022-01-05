c371fd6fb534f5f9c4bbbb3b5f26b5f5b7e772c6
Marco Von Ballmoos <marco@earthli.com>
2022-01-05 17:02:04 +0100

    Add more animation examples

--- app-2021/animation-main.js
+++ app-2021/animation-main.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", async () => {
   const naiveButton = document.getElementById("naive");
   const requestAnimationFrameButton = document.getElementById("requestAnimationFrame");
+  const cssTransitionsButton = document.getElementById("cssTransitions");
+  const cssAnimationsButton = document.getElementById("cssAnimations");
+  const webAnimationsButton = document.getElementById("webAnimations");
 
   naiveButton.addEventListener("click", () => {
     const element = document.getElementById("naiveBlock");
@@ -28,5 +31,43 @@ window.addEventListener("load", async () => {
     }
 
     window.requestAnimationFrame(step);
+  });
+
+  cssTransitionsButton.addEventListener("click", () => {
+    const element = document.getElementById("cssTransitionBlock");
+
+    if (element.style.opacity === "0.3") {
+      element.style.opacity = "1";
+    } else {
+      element.style.opacity = "0.3";
+    }
+  });
+
+  cssAnimationsButton.addEventListener("click", () => {
+    const element = document.getElementById("cssAnimationBlock");
+
+    element.style.animationPlayState = "running";
+
+    if (element.style.animationName === "fadeOut") {
+      element.style.animationName = "fadeIn";
+    } else {
+      element.style.animationName = "fadeOut";
+    }
+  });
+
+  let animation;
+
+  webAnimationsButton.addEventListener("click", () => {
+    const webAnimationBlock = document.getElementById("webAnimationBlock");
+    const options = {
+      duration: 1000,
+      fill: "forwards",
+    };
+    const keyFrames = [{ opacity: 1 }, { opacity: 0.3 }];
+    if (!animation) {
+      animation = webAnimationBlock.animate(keyFrames, options);
+    } else {
+      animation.reverse();
+    }
   });
 });
