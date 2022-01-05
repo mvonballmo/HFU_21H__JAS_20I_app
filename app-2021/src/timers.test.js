28a35f6d5631a11b8a1bc776285994327b957e7a
Marco Von Ballmoos <marco@earthli.com>
2022-01-05 11:52:31 +0100

    Add tests for timers

---
+++ app-2021/src/timers.test.js
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, expect, test } from "@jest/globals";
+import "isomorphic-fetch";
+import { crud } from "./crud";
+
+describe("Timers", () => {
+  test("set timeout of 1/4 second", done => {
+    setTimeout(() => done(), 250);
+  });
+
+  test("clear timeout of 1/4 second", () => {
+    const t = setTimeout(() => {
+      throw new Error("Timeout should not have occurred.");
+    }, 250);
+
+    clearTimeout(t);
+  });
+
+  test("set interval of 1/10 second", done => {
+    let counter = 0;
+
+    const t = setInterval(() => {
+      counter += 1;
+
+      if (counter > 2) {
+        clearTimeout(t);
+        done();
+      }
+    }, 100);
+  });
+});
