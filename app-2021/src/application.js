fdf69774d2dbe44a8bf642608725102b8592c8f6
Marco Von Ballmoos <marco@earthli.com>
2022-01-09 20:51:06 +0100

    Add support for albums

--- app-2021/src/application.js
+++ app-2021/src/application.js
@@ -6,7 +6,7 @@ import "./header.js";
  * @param rootUrl {string}
  */
 export function createMetadata(rootUrl) {
-  return [createAddressMetadata(rootUrl), createCarMetadata(rootUrl)];
+  return [createAddressMetadata(rootUrl), createCarMetadata(rootUrl), createAlbumMetadata(rootUrl)];
 }
 
 /**
@@ -61,6 +61,31 @@ export function createCarMetadata(rootUrl) {
         name: "model",
         type: "text",
         caption: "Model",
+      },
+    ],
+  };
+}
+/**
+ * @return ClassMetadata
+ * @param rootUrl {string}
+ */
+export function createAlbumMetadata(rootUrl) {
+  return {
+    caption: "Album",
+    pluralCaption: "Albums",
+    rootUrl: `${rootUrl}albums`,
+    getTitle: c => `${c.title}`,
+    createNew: () => ({ title: "", description: "" }),
+    properties: [
+      {
+        name: "title",
+        type: "text",
+        caption: "Title",
+      },
+      {
+        name: "description",
+        type: "text",
+        caption: "Description",
       },
     ],
   };
