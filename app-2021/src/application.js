5e002ff9e6111cb54f89b0fccb856096589935b1
Marco Von Ballmoos <marco@earthli.com>
2022-01-09 20:49:05 +0100

    Add support for Address.salary

--- app-2021/src/application.js
+++ app-2021/src/application.js
@@ -31,6 +31,11 @@ export function createAddressMetadata(rootUrl) {
         type: "text",
         caption: "Last Name",
       },
+      {
+        name: "salary",
+        type: "text",
+        caption: "Salary",
+      },
     ],
   };
 }
