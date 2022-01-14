b4b724ca3cc12180a52b4f445f6bb3fbded7594d
Marco Von Ballmoos <marco@earthli.com>
2022-01-14 23:52:48 +0100

    Migrate app from single-file-react to Vite

---
+++ app-2021-vite-js/src/Detail.jsx
@@ -0,0 +1,49 @@
+import { useCallback, useContext, useEffect, useRef } from "react";
+import { AppContext } from "./AppContext";
+import { InputRow } from "./InputRow";
+
+export function Detail() {
+  const { state, service } = useContext(AppContext);
+  const { classMetadata, entity } = state;
+  const saveButton = useRef(null);
+
+  const saveEntity = async e => {
+    await service.saveEntity(state);
+    e.preventDefault();
+  };
+
+  const deleteEntity = async e => {
+    await service.deleteEntity(state);
+    e.preventDefault();
+  };
+
+  const checkValidity = () => {
+    saveButton.current.form.reportValidity();
+  };
+
+  const validate = useCallback(input => (saveButton.current.disabled = !input.form.checkValidity()), []);
+
+  useEffect(() => {
+    validate(saveButton.current);
+  }, [entity]);
+
+  return !entity ? (
+    <div>Nothing selected</div>
+  ) : (
+    <form>
+      {classMetadata.properties.map(p => (
+        <InputRow key={p.name} p={p} validate={validate} />
+      ))}
+      <span>&nbsp;</span>
+      <span>
+        <button ref={saveButton} onClick={saveEntity}>
+          Save
+        </button>
+        <button disabled={!entity.id} onClick={deleteEntity}>
+          Delete
+        </button>
+        <button onClick={checkValidity}>Check</button>
+      </span>
+    </form>
+  );
+}
---
+++ app-2021-vite-js/src/List.jsx
@@ -0,0 +1,18 @@
+import { useContext, useCallback } from "react";
+import { AppContext } from "./AppContext";
+import { Item } from "./Item";
+
+export function List() {
+  const { state } = useContext(AppContext);
+  const { entities } = state;
+
+  const logClick = useCallback(() => console.log("selection changed"), []);
+
+  return (
+    <ul>
+      {entities.map(e => (
+        <Item key={e.id ?? 0} entity={e} log={logClick} />
+      ))}
+    </ul>
+  );
+}
