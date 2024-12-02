ac1ed8ba72aebf63361641e832301063fb463ed1
Marco von Ballmoos <marco@earthli.com>
2024-12-02 11:25:34 +0100

    Convert app to a counter example
    - introduce more functions
    - add more tests
    - runs in the browser (but has an error in the console)

---
+++ app-2024/library.test.js
@@ -0,0 +1,21 @@
+const { test, describe, expect } = require('@jest/globals');
+const library = require('./library');
+
+describe('library', () => {
+  test('counter is initialized to 1', () => {
+    expect(library.counter()).toBe(1);
+  });
+
+  test('increment increases by 1', () => {
+    const currentCounter = library.counter();
+
+    expect(library.increment()).toBe(currentCounter + 1);
+  });
+
+  test('decrement decreases by 1', () => {
+    const currentCounter = library.counter();
+
+    expect(library.decrement()).toBe(currentCounter - 1);
+  });
+})
+
--- app-2024/script.js
+++
@@ -1,4 +0,0 @@
-const n = 1;
-const m = 2;
-
-document.write(n + m * 4);
