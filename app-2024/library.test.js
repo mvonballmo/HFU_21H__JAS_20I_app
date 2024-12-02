30200795cd7bdbcf529d15c856e231b93b0dd1a8
Marco von Ballmoos <marco.vonballmoos@hfu.ch>
2024-12-02 18:48:54 +0100

    Add test for fetching

--- app-2024/library.test.js
+++ app-2024/library.test.js
@@ -1,5 +1,6 @@
 import { test, describe, expect } from "@jest/globals";
 import { counter, increment, decrement } from "./library";
+import "isomorphic-fetch";  // Oben in die Datei
 
 describe("library", () => {
   test("counter is initialized to 1", () => {
@@ -16,5 +17,13 @@ describe("library", () => {
     const currentCounter = counter();
 
     expect(decrement()).toBe(currentCounter - 1);
+  });
+
+  test("call fetch gets addresses", () => {
+    return fetch("http://localhost:3000/addresses")
+      .then(data => data.json())
+      .then(addresses => {
+        expect(addresses.length).toBe(3);
+      });
   });
 });
