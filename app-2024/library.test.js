e2d8f7a985f295e2c0a47217020f7380f18808c3
Marco von Ballmoos <marco@earthli.com>
2024-12-02 11:31:25 +0100

    Convert CommonJS modules to ES6 modules
    The tests run but we can no longer load the index.html from the file
    system. See the next commit for the fix.

--- app-2024/library.test.js
+++ app-2024/library.test.js
@@ -1,21 +1,20 @@
-const { test, describe, expect } = require('@jest/globals');
-const library = require('./library');
+import { test, describe, expect } from "@jest/globals";
+import { counter, increment, decrement } from "./library";
 
-describe('library', () => {
-  test('counter is initialized to 1', () => {
-    expect(library.counter()).toBe(1);
+describe("library", () => {
+  test("counter is initialized to 1", () => {
+    expect(counter()).toBe(1);
   });
 
-  test('increment increases by 1', () => {
-    const currentCounter = library.counter();
+  test("increment increases by 1", () => {
+    const currentCounter = counter();
 
-    expect(library.increment()).toBe(currentCounter + 1);
+    expect(increment()).toBe(currentCounter + 1);
   });
 
-  test('decrement decreases by 1', () => {
-    const currentCounter = library.counter();
+  test("decrement decreases by 1", () => {
+    const currentCounter = counter();
 
-    expect(library.decrement()).toBe(currentCounter - 1);
+    expect(decrement()).toBe(currentCounter - 1);
   });
-})
-
+});
