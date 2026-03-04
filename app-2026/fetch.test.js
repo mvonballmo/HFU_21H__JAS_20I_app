37846f2149bb28bf5d2569894a8526a0234b1064
Marco von Ballmoos <marco@earthli.com>
2026-03-04 21:18:14 +0100

    Add test for POST and DELETE

--- app-2026/fetch.test.js
+++ app-2026/fetch.test.js
@@ -1,11 +1,40 @@
 import { test, describe, expect } from "@jest/globals";
 
 describe("fetch", () => {
-  test("call fetch gets addresses", () => {
-    return fetch("http://localhost:3000/addresses")
-      .then(data => data.json())
-      .then(addresses => {
-        expect(addresses.length).toBe(2);
+  test("call fetch gets addresses", async () => {
+    const response = await fetch("http://localhost:3000/addresses");
+    if (response.ok) {
+      const addresses = await response.json();
+
+      expect(addresses.length).toBe(2);
+    } else {
+      expect(false);
+    }
+  });
+
+  test("create and delete address", async () => {
+    const address = {
+      name: "Bob",
+    };
+
+    let addressWithId;
+
+    try {
+      const response = await fetch("http://localhost:3000/addresses/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(address),
       });
+
+      if (response.ok) {
+        addressWithId = await response.json();
+      }
+    } finally {
+      await fetch(`http://localhost:3000/addresses/${addressWithId.id}`, {
+        method: "DELETE",
+      });
+    }
   });
 });
