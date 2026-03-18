de09638274c3fb2515ae108726c7306c10314bb9
Marco von Ballmoos <marco@earthli.com>
2026-03-18 18:48:44 +0100

    Add event test

--- app-2026/fetch.test.js
+++ app-2026/fetch.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { test, describe, expect } from "@jest/globals";
 import { Address } from "./Address";
 
@@ -15,35 +19,51 @@ async function getAddresses() {
   throw `Response was ${response.statusText}.`;
 }
 
-describe("fetch", () => {
-  test("call fetch gets addresses", async () => {
-    const addresses = await getAddresses();
-    expect(addresses.length).toBe(2);
-  });
+describe("general", () => {
+  describe("fetch", () => {
+    test("call fetch gets addresses", async () => {
+      const addresses = await getAddresses();
+      expect(addresses.length).toBe(2);
+    });
 
-  test("create and delete address", async () => {
-    const address = {
-      name: "Bob",
-    };
+    test("create and delete address", async () => {
+      const address = {
+        name: "Bob",
+      };
 
-    let addressWithId;
+      let addressWithId;
 
-    try {
-      const response = await fetch("http://localhost:3000/addresses/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(address),
-      });
+      try {
+        const response = await fetch("http://localhost:3000/addresses/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(address),
+        });
 
-      if (response.ok) {
-        addressWithId = await response.json();
+        if (response.ok) {
+          addressWithId = await response.json();
+        }
+      } finally {
+        await fetch(`http://localhost:3000/addresses/${addressWithId.id}`, {
+          method: "DELETE",
+        });
       }
-    } finally {
-      await fetch(`http://localhost:3000/addresses/${addressWithId.id}`, {
-        method: "DELETE",
+    });
+
+    describe("events", () => {
+      test("using 'click' event-listener without event", done => {
+        document.body.innerHTML = `<button id="button"/>`;
+
+        const button = document.getElementById("button");
+
+        button.addEventListener("click", () => {
+          done();
+        });
+
+        button.click();
       });
-    }
+    });
   });
 });
